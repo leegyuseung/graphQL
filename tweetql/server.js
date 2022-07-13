c191346c4755944e2d35fe75869ace9ab39ebfc1
leegyuseung <lgs4002@naver.com>
2022-07-13 23:09:16 +0900

    GRAPHQL API

---
+++ tweetql/server.js
@@ -0,0 +1,30 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean
+  }
+`;
+// GET /api/v1/tweets
+// POST /api/v1/tweets
+// GET /api/v1/tweets/:id
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => console.log(`Running on ${url}`));
